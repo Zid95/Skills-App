1ce5cea9bbd0f1ce0e0b1c5d856cfab54b108b78
Zid95 <zid.abdulkarim@gmail.com>
2022-11-30 02:35:46 +0100

    Create Input

---
+++ Skill App.py
@@ -0,0 +1,11 @@
+input_message = """ 
+What Do You Want To Do ?
+"s" => Show All Skills.
+"a" => Add New Skill.
+"d" => Delete A Skill
+"u" => Update Skill Progress
+"q" => Quite The App
+Choose Option: 
+"""
+
+user_input = input(input_message).strip().lower()
